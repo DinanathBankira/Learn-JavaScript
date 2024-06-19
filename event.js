28a6234882221a5fd6653161c89925f98c192f89
Dina <dinanath88090@outlook.com>
2024-06-19 09:04:00 +0530

    inintial commit

---
+++ event.js
@@ -0,0 +1,5 @@
+function paraClicked(){
+    document.getElementById("abc").setAttribute("style","color:yellow");
+}
+
+document.getElementById("abc").addEventListener("click".paraClicked);
---
+++ note.md
@@ -0,0 +1,15 @@
+String
+Number
+Bigint
+Boolean
+undefined
+null
+symbol
+Object
+
+
+==OBJECTS==
+const persion{
+    firstname:"Dinanath",
+    lastname:"Bankira"
+};
