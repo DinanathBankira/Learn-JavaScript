c761969635598fc22036e0d1a2c4a76cad4dad19
Dina <dinanath88090@outlook.com>
2024-06-21 18:54:18 +0530

    Datatypes

---
+++ variavels.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all JS Code as newer version
+
+
+// alert(2+4); we are using nodejs. note brwoser
+console.log(2+3) ; //code readablity
+console.log("Bankira");
+// Number
+// null
+// String
+// Symbol
+// null
+// BigInt
+// undefined
